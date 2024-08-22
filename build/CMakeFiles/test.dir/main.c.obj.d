5409a7c31851491fbdf0e090e45a370f0d74ba8d
2668350209 <2668350209@qq.com>
2024-08-22 21:58:49 +0800

    cmake_test_example

---
+++ build/CMakeFiles/test.dir/main.c.obj.d
@@ -0,0 +1,8 @@
+CMakeFiles/test.dir/main.c.obj: \
+ C:\Users\Administrator\Desktop\test\main.c \
+ e:\software\mingw\include\stdio.h e:\software\mingw\include\_mingw.h \
+ e:\software\mingw\include\msvcrtver.h e:\software\mingw\include\w32api.h \
+ e:\software\mingw\include\sdkddkver.h \
+ e:\software\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
+ e:\software\mingw\include\sys/types.h \
+ e:\software\mingw\lib\gcc\mingw32\6.3.0\include\stdarg.h
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    printf("Hello, from test!\n");
+    return 0;
+}
+
